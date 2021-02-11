148f5decc2cdca7ee0452ac326727025d7d53571
ScArY <tselmennm@gmail.com>
2021-02-11 17:42:16 +0800

    shoog shiiddeg bolgoh

--- app.js
+++ app.js
@@ -8,18 +8,20 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
-
-//<div class="player-score" id="score-0">43</div>
-// window.document.querySelector("#score-0").textContent = dice;
-
-// document.querySelector("#score-1").innerHTML = dice;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 //program ehlehed beltgey
-document.querySelector("#score-0").textContent = "0";
-document.querySelector("#score-1").textContent = "0";
-document.querySelector("#current-0").textContent = "0";
-document.querySelector("#current-1").textContent = "0";
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 
-console.log("Шоо :" + dice);
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
